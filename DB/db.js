f58ef5e16e2c635299a43063a3ab8ef4c806e1b5
sauravpandey538 <sauravpandey0325@icloud.com>
2024-04-14 19:54:10 +0545

    sfg

--- DB/db.js
+++ DB/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://sauravpandey0325:murTIYWmGpDgPYY4@cluster0.7rjfole.mongodb.net/texaschat", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect("mongodb+srv://sauravpandey0325:murTIYWmGpDgPYY4@cluster0.7rjfole.mongodb.net/texaschat");
         console.log(`MongooseDB is connected successfully`);
     } catch (error) {
         console.error("Database connection failed", error);
